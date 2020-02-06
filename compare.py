c54dc7c21b91a2170a7b35fba8905175ff2db0b5
smarsu <754247874@qq.com>
2020-02-06 12:45:06 +0800

    first commit

--- compare.py
+++ compare.py
@@ -23,7 +23,7 @@ def load(path):
         return data
 
 
-def _norm(self, x, eps=1e-6):
+def _norm(x, eps=1e-6):
     """
     Args:
         x: ndarray
@@ -46,6 +46,8 @@ def cosine_similarity(x, y):
         score: float. The cosine similarity of x and y, belong to [-1, 1]
     """
     assert x.shape == y.shape
+    x = x.astype(np.float32)
+    y = y.astype(np.float32)
 
     x = _norm(x)
     y = _norm(y)
@@ -53,20 +55,40 @@ def cosine_similarity(x, y):
     return score
 
 
+def maer(x, y, eps=1e-6):
+    """Mean abs error rate
+    
+    Args:
+        x: ndarray
+        y: ndarray. The shape of y shape equal to x.
+        eps: float. To avoid div 0.
+
+    Returns:
+        score: float. Mean abs error rate, [0, INF]
+    """
+    score = np.mean(np.abs(x - y) / (np.abs(y) + eps))
+    return score
+
+
 def compare_all(x, y):
     """
     Args:
         x: ndarray
-        y: ndarray. The shape of y shape equal to x.
+        y: ndarray. The shape of y shape equal to x. y should be the ground truth.
     """
     assert x.shape == y.shape
 
     cos_sim = cosine_similarity(x, y)
     glog.info('cos_sim: {}'.format(cos_sim))
 
+    meam_abs_error_rate = maer(x, y)
+    glog.info('meam_abs_error_rate: {}'.format(meam_abs_error_rate))
+
 
 if __name__ == '__main__':
     if (not len(sys.argv) == 3):
       raise IOError('Usage: python3 {} path1 path2'.format(sys.argv[0]))
 
-  
+    x = load(sys.argv[1])
+    y = load(sys.argv[2])
+    compare_all(x, y)
